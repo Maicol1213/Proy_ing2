79ab183eb9e70e7872c252eed04cd99d598e6886
georges.paez <georgesp610@gmail.com>
2024-04-17 10:14:21 -0500

    proyecto base

---
+++ src/main/java/co/ucentra/citbanco/Citasbanc/CitasbancApplication.java
@@ -0,0 +1,13 @@
+package co.ucentra.citbanco.Citasbanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasbancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasbancApplication.class, args);
+	}
+
+}
